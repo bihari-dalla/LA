8689aebb1fa6a8f5556fd3a9da73266056161b79
bihari-dalla <firstyeardatascience@gmail.com>
2026-04-04 00:06:56 +0530

    la-all.py

---
+++ la-all.py
@@ -0,0 +1,12 @@
+'''
+p1 : https://tinyurl.com/la-operations
+p2 : https://tinyurl.com/la-det
+p3 : https://tinyurl.com/la-vec
+p4 : https://tinyurl.com/la-echelon
+p5 : https://tinyurl.com/la-ortho
+p6 : https://tinyurl.com/la-schmidt
+p7 : https://tinyurl.com/la-eigenmodal
+p8 : https://tinyurl.com/la-chara
+p9 : https://tinyurl.com/la-derog
+p10 : https://tinyurl.com/la-eqn
+'''
